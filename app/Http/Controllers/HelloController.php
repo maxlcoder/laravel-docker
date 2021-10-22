4c52176ee6520c2b70e0567474207f7c6874d7a9
Renlin Liu <liurenlin@abukito.com>
2021-10-22 19:16:23 +0800

    update

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -2,16 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class HelloController extends Controller
 {
     public function hello(Request $request) {
-        return 'hello world';
+        $name = $request->get('name');
+        if (Cache::get('user_name') == $name) {
+            return 'Redis 检查， ' . $name . ' 已被找到';
+        }
+        $user = User::query()
+            ->where('name', $name)
+            ->first();
+        if ($user) {
+            Cache::put('user_name', $name, 60);
+            return $name . ' 已被找到';
+        }
+        return $name . ' 未找到';
     }
 
     public function say(Request $request)
     {
-        return 'hello';
+        $name = $request->get('name');
+        if (Cache::get('user_name') == $name) {
+            return 'Redis 检查， ' . $name . ' 已被创建';
+        }
+        $user = User::query()
+            ->firstOrCreate([
+                'name' => 'fdsfdsf',
+            ]);
+        Cache::put('user_name', $name, 60);
+        return $name . ' 创建完成';
     }
 }
